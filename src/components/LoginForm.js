4ac7aedfe8100adf7f330a12a8fbed519856acd5
github username <github email address>
2018-11-19 23:56:36 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:'',
+      password:'',
+    }
+  };
+
+ handleInputChange = (e) => {
+   this.setState({
+     [e.target.name]: e.target.value,
+   }) 
+ };
+
+ checkFields = (e) => {//check
+  e.preventDefault()
+  console.log("i am here I am working ")
+  if(!this.state.username ||  !this.state.password){
+    return 
   }
+   this.props.onSubmit(this.state)
+ };//check edn 
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.checkFields}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,29 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      tweet: '',
+      remainingChars: props.maxChars
+    };
+  }
+
+  updateTweet = (e) => {
+    const tweet = e.target.value;
+    this.setState({
+      tweet,
+      remainingChars: this.props.maxChars - tweet.length
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.tweet} onChange={this.updateTweet} />
+        <h4>Your reamining characters are: {this.state.remainingChars}</h4>
       </div>
     );
   }
